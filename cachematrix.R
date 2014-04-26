f2ac99cf13b8dee8554d1d9d47eade36528a9bfe
Jorge Hidalgo <jorhidalgo@yahoo.com>
2014-04-26 18:06:35 -0400

    final version

--- cachematrix.R
+++ cachematrix.R
@@ -1,7 +1,13 @@
-## Jorge Hidalgo Put comments here that give an overall description of what your
-## functions do
+## Jorge Hidalgo - R Programming Assignment 
+## The function makeCacheMatrix creates a list with 4 functions and saves in cache the inverse matrix  
+## of the matrix passed as parameter
+## The function cacheSolve has as a parameter the list created by makeCacheMatrix and returns the inverse matrix
+## cacheSolve will calculate the inverse matrix only if it was not calculate previously
 
-## Write a short comment describing this function
+## The makeCacheMatrix function receives as parameter a matrix and returns a list with 4 functions
+##  The makeCacheMatrix also calculates the inverse of the matrix passed as parameter and caches the result using 
+## the operator <<
+
 
 makeCacheMatrix <- function(x = matrix ()) {
   m <- NULL
@@ -10,24 +16,26 @@ makeCacheMatrix <- function(x = matrix ()) {
     m <<- NULL
   }
   get <- function() x
-  setsolve <- function(solve) m <<- solve
+  setsolve <- function(solve) m <<- solve                     ## the inverse matrix is calculated and saved in cache  
   getsolve <- function() m
-  list(set = set, get = get,
+  list(set = set, get = get,                                  ## create list with 4 functions 
        setsolve = setsolve,
        getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## The cacheSolve functions first validates if it has stored in $getsolve a value different from null
+## if the value is different from null then retrieves the inverse matrix that was already stored
+## if the value in $getsolve is null then calculates the inverse matrix 
 
 cacheSolve <- function(x, ...) {
-  m <- x$getsolve()
+  m <- x$getsolve()                                               ## retrieves from the list the value stores in $getsolve
   if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
+                     message("getting cached inverse matrix")     ## print message it is using cached inverse matrix
+                      return(m)                                   ## returns the value that was already stored
   }
-  data <- x$get()
-  m <- solve(data, ...)
+  data <- x$get()                                        
+  m <- solve(data, ...)                                           ## inverse matrix not in cache then calculates it
   x$setsolve(m)
-  m
+  m                                                               ## returns calculate value of inverse matrix 
 }
